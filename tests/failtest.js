7416347ff4a38714d6fce7cb12e4dc75f831784f
Jaryt <Jarytbustard@gmail.com>
2019-10-30 16:36:17 -0400

    Added a failtest and moved tests to own file
    Created failtest.js to ensure that it wasn't just passing. (The test passed on the first try so I had to make sure, and figured it be nice to keep it)

---
+++ tests/failtest.js
@@ -0,0 +1,10 @@
+import { Selector } from 'testcafe';
+
+fixture `index`
+    .page `127.0.0.1:8080`;
+
+test('Button Test', async t => {
+    await t
+        .click('#testButton')
+        .expect(Selector('#textHolder').innerText).eql('goodbye world!');
+});
