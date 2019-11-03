b6188544fb9e5ac8cff45518653ead655b732421
Jaryt <Jarytbustard@gmail.com>
2019-11-02 21:31:39 -0500

    Final test!

--- tests/test.js
+++ tests/test.js
@@ -3,8 +3,8 @@ import { Selector } from 'testcafe';
 fixture `index`
     .page `127.0.0.1:8080`;
 
-test('The button should add "hello circleci!" to the page', async t => {
+test('The button should add "Hello CircleCI!" to the page', async t => {
     await t
         .click('#testButton')
-        .expect(Selector('#textHolder').innerText).eql('hello circleci!');
+        .expect(Selector('#textHolder').innerText).eql('Hello CircleCI!');
 });
