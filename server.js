3dd773fd3f653ff7241e1da855052a8a179fd242
Jaryt <Jarytbustard@gmail.com>
2019-11-02 21:25:35 -0500

    testing 8080

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
