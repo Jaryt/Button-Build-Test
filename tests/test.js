3ea71d997433f276406a11f83b5f2c71f7a08dea
Jaryt <Jarytbustard@gmail.com>
2019-11-02 20:17:55 -0400

    test results test

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@ import { Selector } from 'testcafe';
 fixture `index`
     .page `127.0.0.1:8080`;
 
-test('Button Test', async t => {
+test('The button should add "hello world" to the page', async t => {
     await t
         .click('#testButton')
         .expect(Selector('#textHolder').innerText).eql('hello world!');
